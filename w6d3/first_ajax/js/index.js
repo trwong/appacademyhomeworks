8ffac5145f1d768a75262c95d910b15889e4d37f
Taylor Wong <trwong93@gmail.com>
2017-11-07 22:21:57 -0800

    Initial Commit w6d3

---
+++ w6d3/first_ajax/js/index.js
@@ -0,0 +1,18 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(data) {
+    console.log('Success!');
+    console.log(data);
+  },
+  error() {
+    console.log('Fail');
+  }
+});
+
+// Add another console log here, outside your AJAX request
+console.log('Console log after the AJAX request');
