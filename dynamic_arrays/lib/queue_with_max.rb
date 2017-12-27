7f98966fb1b6e9ae82fcb145cd42e92fc8fda47e
Taylor Wong <trwong93@gmail.com>
2017-12-26 23:36:34 -0800

    Complete dynamic arrays

--- dynamic_arrays/lib/queue_with_max.rb
+++ dynamic_arrays/lib/queue_with_max.rb
@@ -13,11 +13,16 @@ class QueueWithMax
 
   def initialize
     @array = RingBuffer.new
-    @sorted_array = RingBuffer.new
   end
 
   def enqueue(val)
-    @array.push(val)
+    if @array.length.zero?
+      @array.push([val, val])
+    elsif @array[@array.length - 1][1] > val
+      @array.push([val, @array[@array.length - 1][1]])
+    else
+      @array.push([val, val])
+    end
   end
 
   def dequeue
@@ -25,10 +30,52 @@ class QueueWithMax
   end
 
   def max
+    p @array
+    @array[@array.length - 1][1]
+
   end
 
   def length
     @array.length
   end
-
 end
+
+# Previous Version of code
+  # def enqueue(val)
+  #   prev_max = @max_array[length - 1]
+
+  #   if @max_array.length.zero?
+  #     @max_array.push(val)
+  #   elsif prev_max > val
+  #     @max_array.push(prev_max)
+  #   else
+  #     @max_array.push(val)
+  #   end
+
+  #   @array.push(val)
+  #   # p "enqueue"
+  #   # p @array
+  #   # p "max"
+  #   # p @max_array
+  # end
+
+  # def dequeue
+  #   @max_array.pop
+  #   @array.pop
+  #   p "dequeue"
+  #   p @array
+  #   p "max"
+  #   p @max_array
+  # end
+
+  # def max
+  #   p "length"
+  #   p length
+  #   p "max val"
+  #   p @max_array[length - 1]
+  # end
+
+  # def length
+  #   @array.length
+  # end
+
--- dynamic_arrays/lib/ring_buffer.rb
+++ dynamic_arrays/lib/ring_buffer.rb
@@ -5,40 +5,37 @@ class RingBuffer
 
   def initialize
     @length = 0
-    @array = StaticArray.new(@length)
     @capacity = 8
+    @store = StaticArray.new(@capacity)
     @start_idx = 0
   end
 
   # O(1)
   def [](index)
     check_index(index)
-    @array[(index + @start_idx) % @capacity]
+    @store[(index + @start_idx) % @capacity]
   end
   
   # O(1)
   def []=(index, val)
     check_index(index)
-    @array[index] = val
+    @store[index] = val
   end
 
   # O(1)
   def pop
     raise "index out of bounds" if @length <= 0
     @length -= 1
-    @array[(@start_idx + @length) % @capacity]
+    prev_val = @store[(@start_idx + @length) % @capacity]
+    @store[(@start_idx + @length) % @capacity] = nil
+    prev_val
   end
 
   # O(1) ammortized
   def push(val)
     @length += 1
-    if @length > @capacity
-      @capacity *= 2 
-      # if @start_idx + @length > @capacity / 2
-        
-      # end
-    end
-    @array[(@length + @start_idx - 1) % @capacity] = val
+    resize!
+    @store[(@length + @start_idx - 1) % @capacity] = val
   end
 
   # O(1)
@@ -46,15 +43,15 @@ class RingBuffer
     raise "index out of bounds" if @length <= 0
     @start_idx = (@start_idx + 1) % @capacity
     @length -= 1
-    @array[(@start_idx - 1) % @capacity]
+    @store[(@start_idx - 1) % @capacity]
   end
 
   # O(1) ammortized
   def unshift(val)
     @length += 1
-    @capacity *= 2 if @length > @capacity
+    resize!
     @start_idx = (@start_idx - 1) % @capacity
-    @array[@start_idx] = val
+    @store[@start_idx] = val
   end
 
   protected
@@ -66,5 +63,14 @@ class RingBuffer
   end
 
   def resize!
+    if @length > @capacity
+      new_store = StaticArray.new(@capacity * 2)
+      @length.times do |i|
+        new_store[i] = @store[(i + @start_idx) % @capacity ]
+      end
+      @start_idx = 0
+      @capacity *= 2
+      @store = new_store
+    end
   end
 end
